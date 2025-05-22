c4e965e5c702b2bd57c08945c2bc04272d0b879e
HoangGia <tranlehoanggia2006@gmail.com>
2025-05-22 23:16:45 +0700

    Math supporting function written as namespace

---
+++ MathSupport.h
@@ -0,0 +1,23 @@
+
+#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace MathSupport
+{
+   // get length of a 2D vector
+   float Length(const sf::Vector2f& vec)
+   {
+        float fLength = sqrt(vec.x * vec.x + vec.y * vec.y);
+        return fLength;
+   }
+
+   // take unit vector  
+   sf::Vector2f Normalize(const sf::Vector2f& vec)
+   {
+         float fLength = Length(vec);
+         sf::Vector2f vUnit(vec.x / fLength, vec.y / fLength);
+         return vUnit;
+    }
+}
+
+
